1929a02d68117997d7ac00e41a472015e42ce6dd
ngoctan1234 <ngoctannew@gmail.com>
2023-08-12 12:07:33 +0700

    update 2

--- src/components/Students.js
+++ src/components/Students.js
@@ -2,8 +2,8 @@ export default function Students(){
 
     return (
         <div>
-            <h1>Danh sách sinh viên</h1>
-            
+            <h1>Danh sách sinh viên </h1>
+            <h2>Loài Cá</h2>
         </div>
     )
 }
